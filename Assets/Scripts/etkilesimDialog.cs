faa82803ef4f72baaf78b449129c0ceaa76b9663
Rabia Calik <115617420+rabiacalik@users.noreply.github.com>
2023-04-09 23:38:08 +0300

    etkilesimliDialog ekrani eklendi
    bir şey bir şey

---
+++ Assets/Scripts/etkilesimDialog.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro; // text mash pro icin gerekli
+using UnityEngine.UI;
+
+public class etkilesimDialog : MonoBehaviour
+{
+    public TextMeshProUGUI textComponent; //metin bileseni
+    public string[] lines; //cizgiler
+    public float textSpeed; //yazi hizi
+    private int index;
+    private GameObject panel;
+
+    void Start()
+    {
+        panel = GameObject.Find("etkilesimliPanel");
+        textComponent.text = string.Empty;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "Player")
+        {
+            StartDialogue();
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (textComponent.text == lines[index])
+            {
+                NextLine();
+            }
+            else
+            {
+                StopAllCoroutines();
+                textComponent.text = lines[index];
+            }
+        }
+    }
+    void StartDialogue()
+    {
+        index = 0;
+        StartCoroutine(TypeLine());
+    }
+    IEnumerator TypeLine()
+    {
+        foreach (char c in lines[index].ToCharArray())
+        {
+            textComponent.text += c;
+            yield return new WaitForSeconds(textSpeed);
+        }
+    }
+
+    void NextLine()
+    {
+        if (index < lines.Length - 1)
+        {
+            index++;
+            textComponent.text = string.Empty;
+            StartCoroutine(TypeLine());
+        }
+        else
+        {
+            panel.SetActive(false);
+            gameObject.SetActive(false);
+        }
+    }
+}
